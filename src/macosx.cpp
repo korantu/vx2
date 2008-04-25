6465e568513f63a9c8dfa62781782838109d8129
Konstantin.Levinski <konstantin.levinski@gmail.com>
2008-04-25 16:54:07 +0800

    MacOSX interface

--- src/macosx.cpp
+++ src/macosx.cpp
@@ -2,7 +2,12 @@
 #include <string.h>
 #include <string>
 
-std::string GetOpenFileFromUser(void)
+enum dialog_type {
+  DO_LOAD, DO_SAVE
+};
+
+
+std::string GetOpenFileFromUser(dialog_type d_type)
 {
   NavDialogCreationOptions dialogOptions;
   NavDialogRef dialog;
@@ -10,6 +15,10 @@ std::string GetOpenFileFromUser(void)
   CFURLRef fileAsCFURLRef = NULL;
   FSRef fileAsFSRef;
   OSStatus status;
+    CFIndex sz;
+    CFRange r;
+  
+  std::string res;
 
   // Get the standard set of defaults
   status = NavGetDefaultDialogCreationOptions(&dialogOptions);
@@ -17,9 +26,12 @@ std::string GetOpenFileFromUser(void)
 
   // Make the window app-wide modal
   dialogOptions.modality = kWindowModalityAppModal;
+  dialogOptions.optionFlags |= kNavDontConfirmReplacement;
 
   // Create the dialog
-  status = NavCreateGetFileDialog(&dialogOptions, NULL, NULL, NULL, NULL, NULL, &dialog);
+  status = (DO_LOAD == d_type)?
+    NavCreateGetFileDialog(&dialogOptions, NULL, NULL, NULL, NULL, NULL, &dialog):
+    NavCreatePutFileDialog(&dialogOptions,  kNavGenericSignature, kNavGenericSignature, NULL, NULL, &dialog);
   require_noerr( status, CantCreateDialog );
 
   // Show it
@@ -39,7 +51,30 @@ std::string GetOpenFileFromUser(void)
         
   // Convert it to a CFURL
   // fileAsCFURLRef = CFURLCreateFromFSRef(NULL, &fileAsFSRef);
-
+  //success, lets extraxct info:
+  // kdl;
+ 
+ unsigned char buf[100];
+  buf[0]=0; //null terminated
+  FSRefMakePath( &fileAsFSRef, (UInt8 *)buf, 100); 
+  res = std::string((char *)buf, strlen((char *)buf));
+  
+  if(DO_SAVE == d_type){ //then add file name to the path.
+    r.location = 0;
+    r.length = CFStringGetLength(replyRecord.saveFileName);
+    CFStringGetBytes(
+		     replyRecord.saveFileName,
+		     r,
+		     kCFStringEncodingUTF8,
+		     0,     //dont want to deal with uncovertable strings
+		     false, //internal representation
+		     buf,
+		     100,
+		     &sz);
+    res += "/";
+    res += std::string((char *)buf, sz);		     
+  };
+  
   // Cleanup
  CantExtractFSRef:
  UserCanceled:
@@ -49,15 +84,14 @@ std::string GetOpenFileFromUser(void)
   NavDialogDispose(dialog);
  CantCreateDialog:
  CantGetNavOptions:
-  // kdl;
-  unsigned char buf[100];
-  buf[0]=0; //null terminated
-  FSRefMakePath( &fileAsFSRef, (UInt8 *)buf, 100); 
-  return std::string((char *)buf, strlen((char *)buf));
-  
+  return res;
 };
 
 std::string getFile(){
-  return GetOpenFileFromUser();
+  return GetOpenFileFromUser(DO_LOAD);
+};
+
+std::string putFile(){
+  return GetOpenFileFromUser(DO_SAVE);
 };
 
